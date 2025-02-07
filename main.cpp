fe6cf510964012ce55f07744d960c9791936839a
Diego Juarez <diegojuarez@ucr-secure-31-10-13-175-224.wnet.ucr.edu>
2025-02-07 15:50:18 -0800

    testing

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    cout << "This program will use different algorithms to solve the 8 tile puzzle." << endl;
+    
+    return 0;
+
+}
